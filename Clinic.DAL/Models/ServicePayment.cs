1696c60c50b877926c4ac6e2899ce29b4a8c1f6a
Roman200118 <karsroman2001@mail.ru>
2023-02-22 22:59:46 +0300

    Added db entities

---
+++ Clinic.DAL/Models/ServicePayment.cs
@@ -0,0 +1,18 @@
+﻿namespace Clinic.DAL.Models
+{
+    public class ServicePayment
+    {
+        public int Id { get; set; }
+
+        public int Paid { get; set; }
+
+        public int ServiceId { get; set; }
+
+        public int ClientId { get; set; }
+
+
+        public Client Client { get; set; }
+
+        public Service Service { get; set; }
+    }
+}
